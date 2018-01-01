fa2ad01d144172b523e2ae29cf0e6d36fac089b8
Shanon Place <Shanon.Place@verndale.com>
2017-12-31 19:21:48 -0500

    spelling fix and additions to the README file

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 
 ## Script used to determine the overall health of the git repo and if standards are being followed
 
+## Running the script
+
+`clone the script in git bash to /c/Projects/git-health`
+
+`/c/Projects/git-health/git-health.sh | tee /c/tmp/git-results.txt && code /c/tmp/git-results.txt`
+
 Shanon Place
 shanon.place@verndale.com
--- git-health.sh
+++ git-health.sh
@@ -22,12 +22,18 @@ git checkout develop --quiet  && git pull --quiet
 printf "Remote branches that have already been merged into develop.\n"
 printf "***********************************************************************\n"
 for branch in `git branch -r --merged | cut -c 3- | grep -v HEAD | grep -v "origin/develop"`; do echo -e `git show --format="%ci %cr %an" $branch | head -n 1` \\t$branch | sort -r; done
+printf "Remote branchces can deleted using 'git push origin :branchname'\n"
+print "You can the clean up your local using 'git fetch --all --prune'\n"
 printf "\n"
 
 
 printf "Remote branches that have already NOT been merged into develop.\n"
 printf "***********************************************************************\n"
 for branch in `git branch -r --no-merged | cut -c 3- | grep -v HEAD | grep -v "origin/develop"`; do echo -e `git show --format="%ci %cr %an" $branch | head -n 1` \\t$branch; done
+printf "\n"
+printf "If these branches are old or no longer needed then they should be deleted\n"
+printf "Remote branches can deleted using 'git push origin :branchname'\n"
+printf "You can the clean up your local using 'git fetch --all --prune'\n"
 printf "\n"
 
 
@@ -39,7 +45,16 @@ for branch in `git branch -a | grep -v HEAD | grep -v develop | cut -c 3- | sed 
     printf "$branch \t $countBehind\n"
 done
 printf "\n"
-
+printf "These branches do not have the latest develop commits in them.  If they are still being used then they should be updated with the latest develop\n"
+printf "branches can be updated using the following commands\n"
+printf "git checkout develop\n"
+printf "git pull\n"
+printf "git checkout <branchname>\n"
+printf "git pull\n"
+printf "git merge develop\n"
+printf "If that merged clean you can push it back to remote using\n"
+printf "git push origin <branchname>\n"
+printf "\n"
 
 printf "Commits messages that don't include the Jira ticket and/or branch name\n"
 printf "***********************************************************************\n"
